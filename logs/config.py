b4587d1816724ce77435596bad37321cc0138c5d
Chaim <c0507634153@gmail.com>
2026-06-18 10:15:21 +0300

    Change file's model to 'w'

--- logs/config.py
+++ logs/config.py
@@ -2,6 +2,6 @@ import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s | %(levelname)s | %(message)s',
-                    handlers=(logging.StreamHandler(), logging.FileHandler('logs/app.log')))
+                    handlers=(logging.StreamHandler(), logging.FileHandler('logs/app.log', 'w')))
 
 logger = logging.getLogger()
